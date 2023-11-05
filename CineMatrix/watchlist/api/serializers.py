939ce834e4570e092ceb987bd66523a50a570c48
PRANSHU GUPTA <gpranshu482@gmail.com>
2023-11-05 20:43:01 +0530

    added validations

--- CineMatrix/watchlist/api/serializers.py
+++ CineMatrix/watchlist/api/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 from watchlist.models import Movie
 
-def equality_check(value):
-    if len(value) < 5:
-        raise serializers.ValidationError("Description should be atleast 5 characters long")
-    return data
+
 class MovieSerializer(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
     name=serializers.CharField()
-    description=serializers.CharField(validators=[equality_check])
+    description=serializers.CharField()
     active=serializers.BooleanField()
     
     def validate_name(self,value):
@@ -16,10 +13,10 @@ class MovieSerializer(serializers.Serializer):
             raise serializers.ValidationError("name should be greater than 3 letters")
         return value
     
-    # def validate(self,data):
-    #     if data['name']==data['description']:
-    #         raise serializers.ValidationError("name and description should be different")
-    #     return value
+    def validate(self,data):
+        if data['name']==data['description']:
+            raise serializers.ValidationError("name and description should be different")
+        return value
         
         
         
